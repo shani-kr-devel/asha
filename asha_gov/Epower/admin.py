4d3e4bb9f9c39adf1d3006245056869e94726a87
shani-kr--devel <kumarpc2795@gmail.com>
2026-07-12 23:57:51 +0530

    forms added

--- asha_gov/Epower/admin.py
+++ asha_gov/Epower/admin.py
@@ -1,5 +1,25 @@
 from django.contrib import admin
-from .models import Epower
+from .models import Epower,UserReview,shope,UserProfile
 # Register your models here.
+class ReviewInline(admin.TabularInline):
+    model=UserReview
+    extra=2
+class Epdata(admin.ModelAdmin):
+    list_display=('name','type','date')
+    inlines=[ReviewInline]
+
+class storeadmin(admin.ModelAdmin):
+    list_display=('user','epower')
+
+class userP_admin(admin.ModelAdmin):
+    list_display=('user','address')
+
+
 admin.site.register(Epower)
+admin.site.register(ReviewInline)
+admin.site.register(Epdata)
+admin.site.register(storeadmin)
+admin.site.register(userP_admin)
+
+
 
